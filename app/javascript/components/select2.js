5ebcebd001c3f1c64de31531004060dff104154f
uwusense <matiss0303@gmail.com>
2024-12-22 00:23:23 +0200

    Fix - select2

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -1,28 +1,18 @@
 let isInitialized = false;
 
 function initializeSelect2() {
-  if(!isInitialized) {
-    $('.sort-select').select2({
-        minimumResultsForSearch: Infinity
-    });
-
-    $('.sort-select').off('change').on('change', function() {
-      this.closest('form').submit();
-    });
-    isInitialized = true;
-
-    $('.select2').select2({
+  $('.sort-select').select2({
       minimumResultsForSearch: Infinity
-    })
-  }
+  });
+
+  $('.sort-select').off('change').on('change', function() {
+    this.closest('form').submit();
+  });
+
+  $('.select2').select2({
+    minimumResultsForSearch: Infinity
+  })
 }
 
 document.addEventListener("DOMContentLoaded", initializeSelect2);
 document.addEventListener("turbo:load", initializeSelect2);
-$(document).on("turbolinks:before-cache", function() {
-  $('.select2').select2('destroy');
-});
-
-$(document).on('turbolinks:load', function() {
-  initializeSelect2
-});
